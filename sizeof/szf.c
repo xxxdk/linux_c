1fe828c650e87ebab0a8c5337a8072a32a44a607
xxxdk <953954661@qq.com>
2018-04-28 00:28:21 -0700

    little

--- sizeof/szf.c
+++ sizeof/szf.c
@@ -11,5 +11,12 @@ int main(int argc, char const *argv[]) {
   printf("str: %lu\n p: %lu\n", sizeof(str), sizeof(p));
   func("test");
 
+  printf("char: %lu\n", sizeof(char));
+  printf("short: %lu\n", sizeof(short));
+  printf("int: %lu\n", sizeof(int));
+  printf("long: %lu\n", sizeof(long));
+  printf("float: %lu\n", sizeof(float));
+  printf("long long: %lu\n", sizeof(long long));
+
   return 0;
 }
