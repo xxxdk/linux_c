45f58b2ac2f92dd25bb12cc09bb9b27db8bb07f8
xxxdk <953954661@qq.com>
2019-03-06 00:06:18 +0800

    add double

--- sizeof/szf.c
+++ sizeof/szf.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-void func(char str_arg[100])
+void func(char *str_arg)
 {
   printf("func: %lu\n", sizeof(str_arg));
 }
 
 int main(int argc, char const *argv[]) {
-  char str[] = "Hello";
+  // char str[] = "Hello";
+  char str[] = {'H','E','L','L','O'};
   char *p = str;
-  printf("str: %lu\n p: %lu\n", sizeof(str), sizeof(p));
-  func("test");
+  printf("str: %lu\n p: %lu\n", sizeof(str), sizeof(p)); //6 8
+  func("test"); // 8
 
-  printf("char: %lu\n", sizeof(char));
-  printf("short: %lu\n", sizeof(short));
-  printf("int: %lu\n", sizeof(int));
-  printf("long: %lu\n", sizeof(long));
-  printf("float: %lu\n", sizeof(float));
-  printf("long long: %lu\n", sizeof(long long));
+  printf("char: %lu\n", sizeof(char));  //1
+  printf("short: %lu\n", sizeof(short));  //2
+  printf("int: %lu\n", sizeof(int));  //4
+  printf("long: %lu\n", sizeof(long));  //8
+  printf("float: %lu\n", sizeof(float));  //4
+  printf("double: %lu\n", sizeof(double));  //4
+  printf("long long: %lu\n", sizeof(long long));  //8
 
   return 0;
 }
